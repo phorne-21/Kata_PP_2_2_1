60b3e82a9ca61249206a9ae5957ec8cdb23dcb42
unknown <lwrow@rambler.ru>
2025-03-07 02:00:31 +0300

    modified MainApp

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,19 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
+      User user5 = new User("User5", "Lastname5", "user5@mail.ru");
+      user5.setCar(new Car("model1", 1));
+      User user6 = new User("User6", "Lastname6", "user6@mail.ru");
+      user6.setCar(new Car("model2", 2));
+      User user7 = new User("User7", "Lastname7", "user7@mail.ru");
+      user7.setCar(new Car("model3", 3));
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(user5);
+      userService.add(user6);
+      userService.add(user7);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
